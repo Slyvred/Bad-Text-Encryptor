620cb89e8800821cbb64bcb8d195abceacd3246a
Rémi Hage <52755677+Slyvred@users.noreply.github.com>
2021-10-13 19:15:37 +0200

    made the code not actual garbage

--- Bad_Text_Encryptor.py
+++ Bad_Text_Encryptor.py
@@ -1,25 +1,45 @@
-input_msg = input("Enter some text: ")
+import os
 
-choice = int(input("\n(1) Encrypt\n(2) Decrypt\nChoose an option: "))
-alpha = " ',.;:/\!?@(){}[]°\"=+-*%0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-key = len(input_msg) % len(alpha)
-output_msg = ""
-
-for i in range(len(input_msg)):
-    output_msg = ""
-    for letter in input_msg:
-        if choice == 1:
+alpha = " ',.;:/\!?@(){}[]°\"=+-_*%0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzàéêëôîùç" #
+def encrypt(text, key):
+    for i in range(len(text)):
+        cipher_text = ""
+        for letter in text:
             if (alpha.find(letter) + key) >= len(alpha):
-                output_msg += alpha[(alpha.find(letter) + key) - len(alpha)]
+                cipher_text += alpha[(alpha.find(letter) + key) - len(alpha)]
             else:
-                output_msg += alpha[alpha.find(letter) + key]
-        elif choice == 2:
-            if (alpha.find(letter) - key) >= len(alpha):
-                output_msg += alpha[(alpha.find(letter) - key) - len(alpha)]
-            elif (alpha.find(letter) - key) < 0:
-                output_msg += alpha[(alpha.find(letter) - key) + len(alpha)]
-            else:
-                output_msg += alpha[alpha.find(letter) - key]
-    input_msg = output_msg
+                cipher_text += alpha[alpha.find(letter) + key]
+        text = cipher_text
 
-print(f"\nThe processed text is: \"{output_msg}\"\n")
+    return cipher_text
+
+def decrypt(text, key):
+    for i in range(len(text)):
+        plain_text = ""
+        for letter in text:
+            if (alpha.find(letter) - key) >= len(alpha):
+                plain_text += alpha[(alpha.find(letter) - key) - len(alpha)]
+            elif (alpha.find(letter) - key) < 0:
+                plain_text += alpha[(alpha.find(letter) - key) + len(alpha)]
+            else:
+                plain_text += alpha[alpha.find(letter) - key]
+        text = plain_text
+    return plain_text
+
+
+def main():
+    input_msg = input("Enter some text: ")
+    choice = int(input("\n(1) Encrypt\n(2) Decrypt\nChoose an option (1/2): "))
+    output = ""
+    key = len(input_msg) % len(alpha)
+    
+    if choice == 1:
+        output = encrypt(input_msg, key)
+    elif choice == 2:
+        output = decrypt(input_msg, key)
+    
+    print(f"\nThe processed text is: \"{output}\"\n")
+    os.system('pause')
+
+if __name__ == "__main__":
+    main()
